2b7c3c159f04ee2bd9855b890d5a8a020f04521f
marwanhaz43 <marwanhaz43@gmail.com>
2021-09-19 15:19:29 +0100

    update_img

--- layouts/default.vue
+++ layouts/default.vue
@@ -2,10 +2,10 @@
   <div class="container">
     <common-navbar />
     <h1 style="border-style: dotted; border-color: green; text-align: center;padding:10px;font-size: 30px;color :red">Contact_me:+212 602-78-11-07</h1>
-    <iframe style="margin-top:10px;  display: block;
+    <iframe id="ifmobile" style="margin-top:10px;  display: block;
   margin-left: auto;
   margin-right: auto;
-  width: 50%;" src="https://giphy.com/embed/1yMfs1NsNIAItbpXzO" width="300" height="300" frameBorder="0" class="giphy-embed" allowFullScreen></iframe>
+  width: 100%;" src="https://giphy.com/embed/1yMfs1NsNIAItbpXzO" width="100%" height="400px" frameBorder="0" class="giphy-embed" allowFullScreen></iframe>
     <Nuxt />
  
   </div>
@@ -18,3 +18,6 @@ export default {
   components: { CommonNavbar }
 }
 </script>
+<style >
+
+</style>
