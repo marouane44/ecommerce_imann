91d1e722be360c94a3ce0f3df165ee380aec9360
marwanhaz43 <marwanhaz43@gmail.com>
2021-11-01 11:34:27 +0100

    last_1/11/2021

--- layouts/default.vue
+++ layouts/default.vue
@@ -2,10 +2,10 @@
   <div class="container">
     <common-navbar />
     <h1 style="border-style: dotted; border-color: green; text-align: center;padding:10px;font-size: 30px;color :red">Contact_me:+212 602-78-11-07</h1>
-    <iframe id="ifmobile" style="margin-top:10px;  display: block;
-  margin-left: auto;
-  margin-right: auto;
-  width: 100%;" src="https://giphy.com/embed/1yMfs1NsNIAItbpXzO" width="100%" height="400px" frameBorder="0" class="giphy-embed" allowFullScreen></iframe>
+
+
+
+
     <Nuxt />
  
   </div>
